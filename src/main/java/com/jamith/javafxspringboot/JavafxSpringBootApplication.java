f7738e494b9913730b323c4c38a44685c22bef29
Jamith Nimantha <jamithnimantha@gmail.com>
2021-10-20 12:01:33 +0530

    Initialized Spring Boot Application

---
+++ src/main/java/com/jamith/javafxspringboot/JavafxSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jamith.javafxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafxSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavafxSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamith/javafxspringboot/JavafxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamith.javafxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
