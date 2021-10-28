43cff16bda6e49eec0aeb9a2595b81a2db1c3fb0
Jamith Nimantha <jamithnimantha@gmail.com>
2021-10-29 02:25:12 +0530

    Created Student Entity

---
+++ src/main/java/com/jamith/javafxspringboot/entiry/Student.java
@@ -0,0 +1,36 @@
+package com.jamith.javafxspringboot.entiry;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Student student = (Student) o;
+        return id != null && Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
